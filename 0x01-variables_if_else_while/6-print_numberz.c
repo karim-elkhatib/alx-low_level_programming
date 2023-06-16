14d9f56be0ef0d8c66d53f94a571f89c01ddc39b
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-16 08:47:02 -0700

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: Alpha printing c program
+* Return: always 0 (success)
+*/
+int main(void)
+{
+int x = 0;
+for (x = 48; x <= 57; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
