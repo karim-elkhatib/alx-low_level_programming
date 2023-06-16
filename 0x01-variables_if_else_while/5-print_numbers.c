7ccd703025b38ae0afd1c336ab0a3e4700aa26f2
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-16 08:33:53 -0700

    c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: Numbers printing c program
+* Return: always 0 (success)
+*/
+int main(void)
+{
+int x = 0;
+for (x = 0; x < 10; x++)
+{
+printf("%d", x);
+}
+printf("\n");
+return (0);
+}
