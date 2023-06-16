a3614723ed4d39f1946085623d5b1c9846cc6ac8
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-16 04:08:39 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
+/*
+ *this is a c program that prints using puts
+*/
 #include <stdio.h>
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
