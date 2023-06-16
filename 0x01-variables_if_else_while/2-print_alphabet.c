bc9058efa9ad6ed12b8bcd0bb1248cd63ecfc25a
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-16 08:14:50 -0700

    c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: Alpha printing c program
+* Return: always 0 (success)
+*/
+int main(void)
+{
+char x = 'a';
+ for(x = 'a'; x <= 'z'; x++)
+   {
+     putchar(x);
+   }
+ putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+  char a = 'a';
+  putchar(a);
+  /*printf("%c %d\n",a,a);
+  a++;
+  printf("%c\n",a);
+  a++;
+  if (a != 'c')
+  printf("%c\n",a);*/
+  return (0);
+}
