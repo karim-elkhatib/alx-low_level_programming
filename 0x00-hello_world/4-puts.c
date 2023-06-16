574984e3e365d40c6bd3c8aced60d3ac93442f11
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-16 04:20:38 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-/*
- *this is a c program that prints using puts
-*/
 #include <stdio.h>
+
+/**
+* main - Entry point
+* Description: a c program that uses the puts function
+* Return: always 0 (success)
+*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
