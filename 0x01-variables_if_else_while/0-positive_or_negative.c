b883e8dd8d73a20e36b2b54029f61079964cf51c
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-16 07:06:53 -0700

    c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
-printf("is negative\n");
+printf("%dis negative\n", n);
 else if (n > 0)
-printf("is positive\n");
+printf("%dis positive\n", n);
 else
-printf("is zero\n");
+printf("%dis zero\n", n);
 return (0);
 }
