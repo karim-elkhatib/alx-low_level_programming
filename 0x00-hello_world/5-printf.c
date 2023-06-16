10f4166a8cb6f480e7a2deeb0cb2f6e9cfb9035e
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-16 04:27:21 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: a c program that uses the puts function
+* Return: always 0 (success)
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
