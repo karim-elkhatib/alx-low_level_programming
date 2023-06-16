7c40750592d0768e513f4fbfcf6ca14647d68ba3
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-16 10:47:40 -0700

    c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: print comp3 printing c program
+* Return: always 0 (success)
+*/
+int main(void)
+{
+int x = 48;
+int i = 48;
+int a = 0;
+int b = 0;
+for (i = 48; i < 58; i++)
+{
+for (x = i; x < 58; x++)
+{
+if (x == i)
+{
+}
+else
+{
+putchar(i);
+putchar(x);
+if (i != 56)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
