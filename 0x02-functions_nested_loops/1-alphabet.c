a04ade4292331598a886c32dde359fe3fd5da88e
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-20 12:50:06 -0700

    func task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,4 @@
-#include <stdio.h>
-/**
-* main - Entry point
-* Description: Alpha printing through a func c program
-* Return: always 0 (success)
-*/
-void print_alphabet(void);
-int main(void)
-{
-print_alphabet();
-return (0);
-}
+#include "main.h"
 /**
  * print_alphabet - utility function
  * Description: a function that prints Alpha
@@ -20,7 +9,7 @@ void print_alphabet(void)
 int x = 0;
 for (x = 97; x <= 122; x++)
 {
-putchar(x);
+_putchar(x);
 }
-putchar('\n');
+_putchar(10);
 }
