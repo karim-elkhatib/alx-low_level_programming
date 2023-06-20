8210815066ba2fb1cd96d07d07007d66cf0776de
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-20 12:59:13 -0700

    func task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,21 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 /**
 * _islower - utility function
 * Description: a function that checks if the alpha is U or L
 * @c: describe the parameter: is a character input for the func
 * Return: 1 if the 'c' lowercase & 0 otherwise
 */
-int _islower(int c)
+int print_sign(int n)
 {
-
-}
-/**
-* main - Entry point
-* Description: Alpha printing through a func X10 times
-* Return: always 0 (success)
-*/
-int main(void)
-{
+if (n > 0)
+return (1);
+else if (n < 0)
+return (-1);
+else
 return (0);
 }
-
