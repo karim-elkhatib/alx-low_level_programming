0aeb45f63183ce8fa0de9e24e380cd55d942b101
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-20 09:17:30 -0700

    func task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* print_alphabet - utility function
+* Description: a function that prints Alpha X10
+* Return: it does not return any thing
+*/
+void print_alphabet_x10(void)
+{
+int x = 0;
+int i = 0;
+for (i = 0; i < 10; i++)
+{
+for (x = 97; x <= 122; x++)
+{
+putchar(x);
+}
+putchar('\n');
+}
+}
+/**
+* main - Entry point
+* Description: Alpha printing through a func X10 times
+* Return: always 0 (success)
+*/
+int main(void)
+{
+print_alphabet_x10();
+return (0);
+}
