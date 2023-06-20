9893b6b9c60ac27149e25eb0b69cc3c1195b3254
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-20 14:52:29 -0700

    func task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* jack_bauer - utility function
+* Description: a function that checks if the alpha is U or L
+* Return: 1 if the 'c' lowercase & 0 otherwise
+*/
+void jack_bauer(void)
+{
+int s1;
+int s2;
+int m1;
+int m2;
+for (m1 = 0; m1 < 3; m1++)
+{
+for (m2 = 0; m2 < 10; m2++)
+{
+for (s1 = 0; s1 < 7; s1++)
+{
+for (s2 = 0; s2 < 10; s1++)
+{
+_putchar(m1 + '0');
+_putchar(m2 + '0');
+_putchar(':');
+_putchar(m3 + '0');
+_putchar(m4 + '0');
+}
+}
+}
+}
+}
