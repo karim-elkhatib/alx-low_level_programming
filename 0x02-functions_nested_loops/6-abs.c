a35511b232681d62ebd5fe75e863012ca5c9b548
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-20 13:19:20 -0700

    func task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_sign - utility function
+* Description: a function that checks if the alpha is U or L
+* @n: describe the parameter: is a character input for the func
+* Return: 1 if the 'c' lowercase & 0 otherwise
+*/
+int _abs(int n)
+{
+if (n < 0)
+n = (-2 * n) + n;
+return (n);
+}
