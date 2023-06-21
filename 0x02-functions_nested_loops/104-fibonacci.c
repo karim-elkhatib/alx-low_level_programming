5688545cd4594d48b187ae571cf3b7bbb1a45485
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-21 15:12:10 -0700

    func

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,7 +12,7 @@ if (!num)
 return (1);
 while (num)
 {
-num = num /10;
+num = num / 10;
 l += 1;
 }
 return (l);
@@ -26,21 +26,21 @@ int main(void)
 {
 unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 int count, i;
- for (count = 1; count <= 98; count ++)
+for (count = 1; count <= 98; count++)
 {
 if (f1o > 0)
-printf("%lu",f1o);
+printf("%lu", f1o);
 i = numLength(mx) - 1 - numLength(f1);
-while(f1o > 0 && i > 0)
+while (f1o > 0 && i > 0)
 {
 printf("%d", 0);
 i--;
 }
 printf("%lu", f1);
 sum = (f1 + f2) % mx;
-sumo = f1o + f2o + (f1 +f2) / mx;
+sumo = f1o + f2o + (f1 + f2) / mx;
 f1 = f2;
-f1o =f2o;
+f1o = f2o;
 f2 = sum;
 f2o = sumo;
 if (count != 98)
