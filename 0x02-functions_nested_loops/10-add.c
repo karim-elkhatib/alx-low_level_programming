ebb44326ea09465d6d46857ca1e6591bc8826100
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-21 11:32:41 -0700

    func

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - utility function
+* Description: a function that checks if the alpha is U or L
+* @a: is a character input for the func
+* @b: is a character input for the func
+* Return: 1 if the 'c' lowercase & 0 otherwise
+*/
+int add(int a, int b)
+{
+int sum = 0;
+sum = a + b;
+return (sum);
+}
