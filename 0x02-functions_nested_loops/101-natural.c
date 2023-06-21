583f81e35f450c03696667fe590ce6f3c8fb47ca
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-21 13:34:36 -0700

    func

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 int i = 0;
-int sum = 0; 
+int sum = 0;
 for (i = 0; i < 1024; i++)
 {
 if (i % 3 == 0 || i % 5 == 0)
 sum = sum + i;
-printf("%d\n", sum);
 }
+printf("%d\n", sum);
 return (0);
 }
 
