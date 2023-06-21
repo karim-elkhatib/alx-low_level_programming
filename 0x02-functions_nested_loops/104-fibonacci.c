832e1f9018dbb826f130325eaf7d8b3d5e1e5ec6
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-21 15:04:06 -0700

    func

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* main - utility functionentry point
+* Description: Natural Num. proj
+* Return: 0 in success & 1 error
+*/
+int main(void)
+{
+unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
+int count, i;
+ for (count = 1; count <= 98; count ++)
+{
+if (f1o > 0)
+printf("%lu",f1o);
+i = numLength(mx) - 1 - numLength(f1);
+while(f1o > 0 && i > 0)
+{
+printf("%d", 0);
+i--;
+}
+printf("%lu", f1);
+sum = (f1 + f2) % mx;
+sumo = f1o + f2o + (f1 +f2) / mx;
+f1 = f2;
+f1o =f2o;
+f2 = sum;
+f2o = sumo;
+if (count != 98)
+printf(", ");
+else
+printf("\n");
+}
+return (0);
+}
