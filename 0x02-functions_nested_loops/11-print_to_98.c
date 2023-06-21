85babb8325402e1dd3dbca307f1add781fbb3119
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-21 11:51:36 -0700

    func

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,10 +10,10 @@ void print_to_98(int n)
 int i = 0;
 if (n > 98)
 for (i = n; i > 98; i--)
-printf("%d, ",i);
+printf("%d, ", i);
 else
 for (i = n; i < 98; i++)
-printf("%d, ",i);
+printf("%d, ", i);
 printf("98\n");
 }
 
