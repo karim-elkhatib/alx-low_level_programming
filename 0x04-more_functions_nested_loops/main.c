ccd6e77c29c554b61a4eee5ecb3ae99c3455338f
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-22 03:21:41 -0700

    func 2

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include "0-isupper.c"
+
+int main (void)
+{
+  int x;
+  x = _isupper('C');
+  printf("%d", x);
+  return (0);
+}
---
+++ 1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_isdigit - is a func
+ *@c: is sn argument
+ *Return: 1 or 0
+*/
+
+int _isdigit(int c)
+{
+ if (c >= 65 && c <= 90)
+    return (1);
+  else
+    return (0);
+}
