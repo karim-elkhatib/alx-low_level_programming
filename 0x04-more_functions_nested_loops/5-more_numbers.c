24e8ac863e5f954af5ee7ec0c0e1b4e2fc7ec4d8
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-22 10:41:03 -0700

    func 2

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,7 +14,12 @@ for (x = 0; x < 10; x++)
 i = 0;
 while (i >= 0 && i < 15)
 {
-if (i >= 10)
+if (i < 10)
+{
+_putchar(i + '0');
+}
+else
+{
 _putchar((i / 10) + '0');
 _putchar((i % 10) + '0');
 }
