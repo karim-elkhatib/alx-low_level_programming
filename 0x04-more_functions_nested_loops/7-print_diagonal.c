20abd148ae20c5ba358e56d2448fbf2c349c7a70
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-22 08:16:52 -0700

    func 2

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_diagonal - is a func
+ *@n: an input
+ *Return: no return
+*/
+
+void print_diagonal(int n)
+{
+int i = 0, j = 0;
+for (i = 1; i <= n; i++)
+{
+for (j = 1; j <= (i - 1); j++)
+_putchar(' ');
+_putchar('\\');
+}
+_putchar('\n');
+}
+
+
