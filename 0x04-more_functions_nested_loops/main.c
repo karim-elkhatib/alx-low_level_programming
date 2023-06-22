79e365a6dac2bdc762c5a2a7ce80d98be2416cc9
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-22 04:26:22 -0700

    func 2

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,20 +1,11 @@
 #include "main.h"
-//#include "3-print_numbers.c"
+#include "4-print_most_numbers.c"
 #include "_putchar.c"
 
-void print_numbers(void)
-{
-int i = 48;
-while (i >= 48 && i <= 58)
-{
-  _putchar(i);
-i++;
-}
-_putchar('\n');
-}
 
 int main (void)
 {
-   print_numbers();
+    print_most_numbers();
     return (0);
+
 }
