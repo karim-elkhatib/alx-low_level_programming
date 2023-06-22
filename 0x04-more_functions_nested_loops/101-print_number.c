08688633c16bbf30aec5bb1890f8e0fe4b5cfbaa
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-22 11:54:18 -0700

    Func 2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,13 +7,13 @@
 */
 void print_number(int n)
 {
- unsigned int num = n;
- if (n < 0)
- {
- _putchar('-');
- num = -num;
- }
- if ((num / 10) > 0)
- print_number(num / 10);
- _putchat((num % 10) + 48);
+unsigned int num = n;
+if (n < 0)
+{
+_putchar('-');
+num = -num;
+}
+if ((num / 10) > 0)
+print_number(num / 10);
+_putchat((num % 10) + 48);
 }
