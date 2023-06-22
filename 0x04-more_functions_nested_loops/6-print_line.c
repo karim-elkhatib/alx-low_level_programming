6341c151c127eec8a1d0d4113e7a2fd5c7fa8788
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-22 07:59:53 -0700

    func 2

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_line - is a func
+ *@n: an input
+ *Return: no return
+*/
+
+void print_line(int n)
+{
+int i =0;
+for (i = 1; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
