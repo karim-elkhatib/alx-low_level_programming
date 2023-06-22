9a563c9871ece52c0892496f84c1ce388f272f6b
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-22 08:21:21 -0700

    func 2

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,8 +12,11 @@ int i = 0, j = 0;
 for (i = 1; i <= n; i++)
 {
 for (j = 1; j <= (i - 1); j++)
+{
 _putchar(' ');
+}
 _putchar('\\');
+_putchar('\n');
 }
 _putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,11 +1,14 @@
 #include "main.h"
-#include "5-more_numbers.c"
+#include "7-print_diagonal.c"
 #include "_putchar.c"
 
 
 int main (void)
 {
-  more_numbers();
+  print_diagonal(0);
+    print_diagonal(2);
+    print_diagonal(10);
+    print_diagonal(-4);
     return (0);
 
 }
