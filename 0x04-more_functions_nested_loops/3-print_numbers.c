68d9cccd0b30b050155d6bc540af4dfa7ad18dd3
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-22 03:57:33 -0700

    func 2

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_numbers - is a func
+ *Return: no return
+*/
+
+void print_numbers(void)
+{
+int i = 0;
+while(i <= (0 + '0') && i >= (9 + '0'))
+{
+_putchar(i + '0');
+_putchar('\n');
+i++;
+}
+}
