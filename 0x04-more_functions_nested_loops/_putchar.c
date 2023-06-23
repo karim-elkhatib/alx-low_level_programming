b6459006c6f7176d127ec1162293b4fd1b43ba13
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-22 22:01:52 -0700

    func2

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: the character to print
- * Return: On success 1 & On error -1 is returned 
+* _putchar - writes the character c to stdout
+* @c: the character to print
+* Return: On success 1 & On error -1 is returned
 */
 int _putchar(char c)
 {
-  return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
