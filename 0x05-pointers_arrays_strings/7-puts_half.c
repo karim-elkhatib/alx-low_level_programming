e929a9f175854ca248c30523eb771067e746be75
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-26 12:50:41 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *puts_half - is a func
+ *@str: an input
+ *Return: no return
+*/
+void puts_half(char *str)
+{
+  int i = 0, len = 0, n = 0;
+
+for (i = 0; *(str + i) != '\0'; i++)
+{
+len++;
+}
+if (len % 2 == 0)
+{
+n = len / 2;
+}
+else
+{
+n = (length_of_the_string - 1) / 2;
+}
+for (i = n; *(str + i) != '\0'; i++)
+{
+_putchar(*(str + i));
+} 
+}
