5adeb1edc271b835342b764b2a9fe7bda0b44721
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-26 13:08:15 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_array - is a func
+ *@a: an input
+ *@n: an input
+ *Return: no return
+*/
+void print_array(int *a, int n)
+{
+int i = 0;
+for (i = 0; i < n; i++)
+{
+if (i < (n - 1))
+printf("%d ,", a[i]);
+else
+printf("%d", a[i]);
+}
+_putchar('\n');
+}
