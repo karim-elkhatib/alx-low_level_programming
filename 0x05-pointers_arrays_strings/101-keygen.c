1cdb2364cafe8bdffe3d09b5fb3a005d53346d50
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-27 03:39:17 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ *main - entery point
+ * Description: non
+ * Return: 0
+*/
+
+int main (void)
+{
+  int sum;
+  cahr c;
+  srand(time(NULL));
+  while (sum <= 2645)
+    {
+      c = rand() % 128;
+      sum += c;
+      putchar(c);
+    }
+  putchar(2772 - sum);
+  return (0);
+}
