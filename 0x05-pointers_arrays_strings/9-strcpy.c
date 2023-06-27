885a1bd1c763cf9f791147de8784889daa4e474e
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-27 03:05:00 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_strcpy - is a func
+ *@dest: an input
+ *@scr: an input
+ *Return: no return
+*/
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+for (i = 0; src[i] != '\0': i++)
+{
+dest[i] = src [i];
+}
+dest[i] = '\0';
+return (dest);
+}
