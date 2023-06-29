3e5427019886252fe03047aa1f77231fbe0d0ad3
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-29 02:27:21 -0700

    betty

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- *_strcmp - is a func
- *@s1: an input
- *@s2: an input
+ *string_toupper - is a func
+ *@str: an input
  *Return: no return
 */
 char *string_toupper(char *str)
 {
-    int i = 0;
-    for (i = 0; str[i] != '\0'; i++)
-    {
-      if (str[i] <= 'z' && str[i] >= 'a')
-        str[i] = str[i] - 32;
-    }
-    return (str);
+int i = 0;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] <= 'z' && str[i] >= 'a')
+str[i] = str[i] - 32;
+}
+return (str);
 }
