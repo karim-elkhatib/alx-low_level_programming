9b82c4cd637642369f744751c79925d1b2a0e19e
karim-elkhatib <karim_elkhatib@outlook.com>
2023-06-29 04:53:08 -0700

    edit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,19 +1,45 @@
 #include "main.h"
+/**
+ *_islower - is a func
+ *@x: an input
+ *Return: no return
+*/
+
+char _islower(char x)
+{
+return (x >= 'a' && x <= 'z');
+}
+/**
+ *isSeparator - is a func
+ *@x: an input
+ *Return: no return
+*/
+
+char isSeparator(char x)
+{
+int i = 0;
+char sep[] = " \t\n,;.!?\"(){}";
+for (i = 0; i < 12; i++)
+{
+if (sep[i] == x)
+return (1);
+}
+return (0);
+}
 
 /**
  *cap_string - is a func
  *@str: an input
  *Return: no return
 */
+
 char *cap_string(char *str)
 {
 int i = 0;
 for (i = 0; str[i] != '\0'; i++)
 {
-if ((str[i] == ' ' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == ',' || str[i] == '\t' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}') && (str[i + 1] <= 'z' && str[i + 1] >= 'a'))
-{
-str[i + 1] = str[i + 1] - 32;
-}
+if (isSeparator(str[i]) && _islower(str[i + 1]))
+str[i + 1] -= 32;
 }
 return (str);
 }
