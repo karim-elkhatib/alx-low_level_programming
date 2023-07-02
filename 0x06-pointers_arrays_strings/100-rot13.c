3e3bf61ac310f660855d5c770fefcf63fa3eac83
karim-elkhatib <karim_elkhatib@outlook.com>
2023-07-02 12:45:00 -0400

    x

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,8 +6,25 @@
  *Return: return c
  */
 char _isalpha(char x)
+{ 
+return ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z'));
+}
+
+/**
+ *fistHalf - is a func
+ *@x: an input
+ *Return: return c
+ */
+char fistHalf(char x)
 {
-    return ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z'));
+    int i = 0, check = 1;
+    char a[] ="abcdefghijklmABCDEFGHIJKLM"; 
+    while(x != a[i] && a[i] != '\0')
+    {
+        check = 0;
+        i++;
+    }
+return (check);
 }
 
 /**
@@ -20,7 +37,7 @@ char *rot13(char *c)
 int i = 0;
 for (i = 0; c[i] != '\0'; i++)
 {
-if (_isalpha(c[i]) && ((c[i] >= 'a' && c[i] <= 'm') || (c[i] >= 'A' && c[i] <= 'M')))
+if (_isalpha(c[i]) && fistHalf(c[i]))
 c[i] += 13;
 else
 {
