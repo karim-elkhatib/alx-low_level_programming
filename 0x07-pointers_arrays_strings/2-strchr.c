07a304047f67c1e19b243a6029e9ffe8482bd703
karim-elkhatib <karim_elkhatib@outlook.com>
2023-07-03 08:05:37 -0400

    xi

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,16 +10,12 @@
 char *_strchr(char *s, char c)
 {
 int i = 0;
-while (*(s + i) != '\0')
+for (i = 0; *(s + i) != '\0'; i++)
 {
 if (*(s + i) == c)
 {
-s = s + i;
-break;
+return (s + i);
 }
-i++;
 }
-if (*(s + i) == '\0')
-s = NULL;
-return (s);
+return('\0');
 }
