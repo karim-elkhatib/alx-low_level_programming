6406a4025b60ab8fb831b371693714a5ce9dbcae
karim-elkhatib <karim_elkhatib@outlook.com>
2023-07-03 07:16:59 -0400

    xi

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strchr - is a func
+ *@s: an input
+ *@c: an input
+ *Return: return c
+ */
+
+char *_strchr(char *s, char c)
+{
+int i = 0;
+while (*(s + i) != '\0')
+{
+    if (*(s + i) == c)
+    {
+        s = s + i;
+        break;
+    }
+i++;
+}
+return (s);
+}
