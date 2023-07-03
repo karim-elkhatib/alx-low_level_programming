7424afdb158537feca113bea3a751fef97c4b08a
karim-elkhatib <karim_elkhatib@outlook.com>
2023-07-03 05:54:43 -0400

    xi

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+
+#include "main.h"
+
+/**
+ *_memset - is a func
+ *@s: an input
+ *@b: an input
+ *@n: an input
+ *Return: return c
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    unsigned int i = 0;
+    while (i < n)
+    {
+        s[i] = b;
+        i++;
+    }
+    return (s);
+}
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *rot13 - is a func
+ *@c: an input
+ *Return: return c
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    
+}
+ 
