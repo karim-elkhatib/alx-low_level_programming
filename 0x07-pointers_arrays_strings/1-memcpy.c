fd458ca3a92d08663d8c1b752d29a65f899af71d
karim-elkhatib <karim_elkhatib@outlook.com>
2023-07-03 06:09:57 -0400

    xi

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,21 @@
+
 #include "main.h"
 
 /**
- *rot13 - is a func
- *@c: an input
+ *_memcpy - is a func
+ *@dest: an input
+ *@src: an input
+ *@n: an input
  *Return: return c
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    
+unsigned int i = 0;
+while (i < n)
+{
+dest[i] = src[i];
+i++;
 }
- 
+return (dest);
+}
