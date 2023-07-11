51ecb880ab39bd3571bd45e4ed2fc0e40c55f07d
karim-elkhatib <karim_elkhatib@outlook.com>
2023-07-11 16:33:18 -0400

    57

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - is a func
+ *@str: an input
+ *Return: return c
+ */
+char *_strdup(char *str)
+{
+if (str == NULL)
+{
+return (NULL);
+}
+else
+{
+char *A;
+unsigned int i = 0, size = 0;
+while (*(str + size) != '\0')
+size++;
+A = (char *)(malloc(sizeof(char) * size));
+while (i < size)
+{
+*(A + i) = *(str + i);
+i++;
+}
+return (A);
+}
+}
