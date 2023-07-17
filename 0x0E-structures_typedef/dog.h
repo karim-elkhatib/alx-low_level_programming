8cfa24b0d61ecc72ef8cf8202552dcd3dae5488a
karim-elkhatib <karim_elkhatib@outlook.com>
2023-07-17 15:54:56 -0400

    57

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,19 @@
-struct dog {
-    char* name;
-    float age;
-    char* owner;
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - pla pla
+ * @name: Arg
+ * @age: Arg
+ * @owner: Arg
+ * Description: pla pla
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
 };
+
+#endif
