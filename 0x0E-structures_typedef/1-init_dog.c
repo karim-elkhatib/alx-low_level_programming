bb34ecd8430fba460ed9051ad7a3972ab59e55f4
karim-elkhatib <karim_elkhatib@outlook.com>
2023-07-17 16:47:38 -0400

    57

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "main.h"
+#include "dog.h"
 /**
  * init_dog - check the code
  * @d: arg
@@ -11,7 +11,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-d->name = *name;
+if (d)
+{
+d->name = name;
 d->age = age;
-d->owner = *owner;
+d->owner = owner;
+}
 }
