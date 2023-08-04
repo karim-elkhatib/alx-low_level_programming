8c5062d5bc88c6c6b4df01815f951f7e468b3b1f
karim-elkhatib <karim_elkhatib@outlook.com>
2023-08-04 13:41:10 -0400

    x

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *main - is a func
+ *@argc: an input
+ *@argv: an input
+ *Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+int i = 0;
+if (argc != 3)
+{
+printf("Error");
+return (1);
+}
+else
+{
+i = argv[1] * argv[2];
+printf("%d\n", i);
+return (0);
+}
+}
