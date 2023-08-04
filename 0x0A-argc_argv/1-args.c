c9c93f29a70e78b6a29249a7531a4ee07cf54e7f
karim-elkhatib <karim_elkhatib@outlook.com>
2023-08-04 13:27:13 -0400

    x

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *main - is a func
+ *@argc: an input
+ *@argv: an input
+ *Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc);
+return (0);
+}
