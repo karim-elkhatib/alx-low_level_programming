06a8b53e14be87a416031cfb2448c44afd1c817a
karim-elkhatib <karim_elkhatib@outlook.com>
2023-08-04 13:21:08 -0400

    x

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- *_strdup - is a func
- *@str: an input
- *Return: return c
+ *main - is a func
+ *@argc: an input
+ *@argv: an input
+ *Return: return 0
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    (void) argc;
-    printf("%s\n", argv[0]);
-    return (0);
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
 }
