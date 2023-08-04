ba2932432985cfcebc7bdcf2b9e67d36963f272e
karim-elkhatib <karim_elkhatib@outlook.com>
2023-08-04 13:14:45 -0400

    x

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_strdup - is a func
+ *@str: an input
+ *Return: return c
+ */
+
+int main(int argc, char* argv[])
+{
+    printf("%s\n", argv[argc]);
+    return (0);
+}
